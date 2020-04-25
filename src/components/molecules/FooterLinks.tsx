f308e59fda1cad2e1c31f6f3c7472ef21374c0d5
ShichidaMasatsugu <shicchi.7913@icloud.com>
2020-04-25 13:26:31 +0900

    ログアウトできるようにする

--- src/components/molecules/FooterLinks.tsx
+++ src/components/molecules/FooterLinks.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import { Forum, PeopleAltRounded, ExitToApp } from '@material-ui/icons';
 
+import { UserSessionContext } from '../../Router';
+
 export const FooterLinks = () => {
+  const { setUserSession } = useContext(UserSessionContext);
   const [current, setCurrent] = useState('messages')
   const handleChange = (_e: React.ChangeEvent<{}>, newCurrent: string) => {
     setCurrent(newCurrent)
+  }
+  const onClickLogout = () => {
+    setUserSession({});
   }
 
   return (
     <BottomNavigation value={current} onChange={handleChange}>
       <BottomNavigationAction component={Link} to='/users' label='Users' value='users' icon={<PeopleAltRounded />} />
       <BottomNavigationAction component={Link} to='/messages' label='Messages' value='messages' icon={<Forum />} />
-      <BottomNavigationAction label='Logout' value='logout' icon={<ExitToApp />}/>
+      <BottomNavigationAction onClick={() => onClickLogout()} label='Logout' value='logout' icon={<ExitToApp />}/>
     </BottomNavigation>
   )
 };
